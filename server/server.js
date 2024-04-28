48a082a59e085fccbc85433072edc71c14fe7021
PraveenSRawatGithub <praveensinghrawat46@gmail.com>
2024-04-28 13:22:04 +0530

    modified server configurations

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import {v2 as cloudinary} from "cloudinary"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
@@ -21,19 +22,25 @@ cloudinary.config({
 
 const app = express()
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve()
+
 app.use(express.json({limit: "5mb"}))
 app.use(express.urlencoded({extended: true}))   // to parse form data(urlEncoded)
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-    res.send("Server is ready")
-})
 
 // routers
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/notifications', notificationRoute)
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
